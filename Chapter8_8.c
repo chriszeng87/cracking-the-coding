5c3359f10143caa5644ab3b4b15f246ef2ad542c
chriszeng87 <chrisgardner87@gmail.com>
2015-05-03 16:53:47 +0800

    add Chapter8_8 solution

---
+++ Chapter8_8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Write an algorithm to print all ways of arranging eight queens on a chess board so that none of
+ * them share the same row, column or diagonal.
+ */
+
+//int queens[8][8] = {0};
+//int row[8] = {-1};
+//int col[8] = {-1};
+
+void rangeQueens(int board[], int n) {
+
+	for (int i = 0; i < 8; i++) {
+
+	}
+}

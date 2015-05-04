14ed39d4570b3118a90c6bfb2ab8416a71ff683f
chriszeng87 <chrisgardner87@gmail.com>
2015-05-05 07:44:24 +0800

    add Chapter 1_8 solution

---
+++ Chapter1_8.java
@@ -0,0 +1,24 @@
+/**
+ * Assume you have a method isSubstring which checks if one word is a substring
+ * of another. Given two strings, s1 and s2, write code to check if s2 is a
+ * rotation of s1 using only one call to isSubstring (i.e., “waterbottle” is a
+ * rotation of “erbottlewat”).
+ * 
+ * @author Chris
+ * 
+ */
+public class Chapter1_8 {
+	
+	// if s2 is substring of s1 + s2, then it is a rotation of s1;
+	// if (s1.length() == s2.length() && s1.length() > 0)
+	//isSubstring(s1 + s1, s2);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
